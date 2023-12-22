7840ef6900837f5bdf07679b5c2fe62c8152db63
jabraham17 <jademabraham17@gmail.com>
2023-12-22 11:15:02 -0800

    add cmake-format to formatter
    Signed-off-by: jabraham17 <jademabraham17@gmail.com>

--- scripts/format.sh
+++ scripts/format.sh
@@ -1,2 +1,3 @@
 #/usr/bin/env bash
 find src include -name '*.c' -o -name '*.h' | xargs clang-format -i
+find CMakeLists.txt src -name CMakeLists.txt | xargs cmake-format -i --command-case=lower --keyword-case=upper --enable-sort=true --autosort=true
