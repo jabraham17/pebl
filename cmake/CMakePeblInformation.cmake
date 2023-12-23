d6bf0583ea857518c9e836a1ed0d490beb723b57
jabraham17 <jademabraham17@gmail.com>
2023-12-23 07:09:02 -0800

    upgrade build script
    Signed-off-by: jabraham17 <jademabraham17@gmail.com>

--- cmake/CMakePeblInformation.cmake
+++ cmake/CMakePeblInformation.cmake
@@ -1,9 +1,9 @@
 set(CMAKE_Pebl_COMPILE_OBJECT
-    "<CMAKE_Pebl_COMPILER> -o <OBJECT> -c <SOURCE> --runtime $ENV{PEBL_SOURCE}/runtime --standard-library $ENV{PEBL_SOURCE}/stdlib"
+    "<CMAKE_Pebl_COMPILER> -o <OBJECT> -c <SOURCE>"
 )
 
 set(CMAKE_Pebl_LINK_EXECUTABLE
-    "<CMAKE_Pebl_COMPILER> -o <TARGET> <OBJECTS> --runtime $ENV{PEBL_SOURCE}/runtime --standard-library $ENV{PEBL_SOURCE}/stdlib"
+    "<CMAKE_Pebl_COMPILER> -o <TARGET> <OBJECTS>"
 )
 
 set(CMAKE_Pebl_ARCHIVE_CREATE "<CMAKE_AR> rcs <TARGET> <OBJECTS>")
