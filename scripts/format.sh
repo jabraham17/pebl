64c48ca35481c83c62c05e5548cc3e08e358aa06
jabraham17 <jademabraham17@gmail.com>
2023-12-23 06:49:32 -0800

    improve format script
    Signed-off-by: jabraham17 <jademabraham17@gmail.com>

--- scripts/format.sh
+++ scripts/format.sh
@@ -1,3 +1,9 @@
 #/usr/bin/env bash
-find src include -name '*.c' -o -name '*.h' | xargs clang-format -i
-find CMakeLists.txt src -name CMakeLists.txt | xargs cmake-format -i --command-case=lower --keyword-case=upper --enable-sort=true --autosort=true
+find src include -name '*.c' -o -name '*.h' | xargs \
+  clang-format -i
+
+find CMakeLists.txt src cmake -name CMakeLists.txt -o -name '*.cmake*' | xargs \
+  cmake-format -i --command-case=lower --keyword-case=upper --enable-sort=true --autosort=true
+
+find src -name '*.py' | xargs \
+  python3 -m black
