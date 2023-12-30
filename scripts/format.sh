577ffb177aa22ff957cf74a2b21d16e8dd1eaf07
jabraham17 <jademabraham17@gmail.com>
2023-12-29 21:07:20 -0800

    make format script run in parallel
    Signed-off-by: jabraham17 <jademabraham17@gmail.com>

--- scripts/format.sh
+++ scripts/format.sh
@@ -1,9 +1,14 @@
 #/usr/bin/env bash
+set -e
+set -x
+
 find src include -name '*.c' -o -name '*.h' | xargs \
-  clang-format -i
+  clang-format -i &
 
 find CMakeLists.txt src cmake -name CMakeLists.txt -o -name '*.cmake*' | xargs \
-  cmake-format -i --command-case=lower --keyword-case=upper --enable-sort=true --autosort=true
+  cmake-format -i --command-case=lower --keyword-case=upper --enable-sort=true --autosort=true &
 
 find src -name '*.py' | xargs \
-  python3 -m black
+  python3 -m black -q &
+
+wait
