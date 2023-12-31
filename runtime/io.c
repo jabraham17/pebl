c24a8033e31fd7de76a59d555bf2a385e45a94bf
jabraham17 <jademabraham17@gmail.com>
2023-12-31 14:58:36 -0800

    expand stdlib
    Signed-off-by: jabraham17 <jademabraham17@gmail.com>

--- runtime/io.c
+++ runtime/io.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+
+__attribute__((noreturn))
+void pebl_panic(char* message) {
+  fprintf(stderr, "%s\n", message);
+  abort();
+}
 
 void* c_openFilePointer(int8_t* name, int8_t* mode) {
   return (void*)fopen((char*)name, (char*)mode);
@@ -19,4 +29,26 @@ void* c_getStdout() {
 }
 void* c_getStdin() {
   return (void*)stdin;
+}
+
+int64_t c_ftell(void* fp) {
+  return ftell((FILE*)fp);
+}
+void c_fseek(void* fp, int64_t offset, int64_t seek_type) {
+  fseek((FILE*)fp, offset, seek_type);
+}
+int64_t c_fseek_seek_type(int64_t seek_type) {
+  if (seek_type == 0) { return SEEK_SET; }
+  else if (seek_type == 1) { return SEEK_CUR; }
+  else if (seek_type == 2) { return SEEK_END; }
+  else { pebl_panic("unknown seek type"); }
+}
+
+int64_t c_pathExists(int8_t* path) {
+  return (access((char*)path, F_OK) == 0);
+}
+int64_t c_pathIsFile(int8_t* path) {
+  struct stat path_stat;
+  if(stat((char*)path, &path_stat) != 0) return 0;
+  return S_ISREG(path_stat.st_mode);
 }
