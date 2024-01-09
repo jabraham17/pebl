5b9791fd3229091e0951c30d0506bc9eb8866527
jabraham17 <jademabraham17@gmail.com>
2024-01-08 20:19:01 -0800

    interpret error codes
    Signed-off-by: jabraham17 <jademabraham17@gmail.com>

--- src/tool/driver/driver/utils.py
+++ src/tool/driver/driver/utils.py
@@ -1,6 +1,8 @@
 import sys
 import subprocess as sp
-from typing import Tuple
+from typing import Optional, Tuple
+import os
+import signal
 
 
 class UnhandledError(Exception):
@@ -28,10 +30,23 @@ def error(*args, **kwargs):
     raise UnhandledError()
 
 
+def _errorcode_tostring(code: int) -> Optional[str]:
+    if code < 0:
+        try:
+            return signal.strsignal(-code)
+        except ValueError:
+            return "unknown error"
+    else:
+        return None
+
+
 def execute_process(*cmd: str) -> Tuple[int, str]:
     log(" ".join(cmd))
     cp = sp.run(cmd, stdout=sp.PIPE, stderr=sp.STDOUT, encoding="utf-8")
-    return (cp.returncode, cp.stdout)
+    output = cp.stdout
+    if extra := _errorcode_tostring(cp.returncode):
+        output += ("\n" if output else "") + extra
+    return (cp.returncode, output)
 
 
 def execute_compiler(*cmd: str) -> None:
